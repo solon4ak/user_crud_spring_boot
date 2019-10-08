4393a3fc38a3a4013f68ed2a6f55a8dcc3ff6704
solon4ak <solon4ak@gmail.com>
2019-10-08 12:51:16 +0300

    added imports to main class

--- src/main/java/ru/solon4ak/Application.java
+++ src/main/java/ru/solon4ak/Application.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
+import ru.solon4ak.configuration.MVCConfig;
 import ru.solon4ak.configuration.SecurityConfiguration;
 
 @SpringBootApplication
 @EntityScan( basePackages = {"ru.solon4ak.model"})
-@Import({SecurityConfiguration.class})
+@Import({SecurityConfiguration.class, MVCConfig.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
